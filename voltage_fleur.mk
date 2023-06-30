d924c48849decc212c3459b65f1a10f0b43d46a9
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-06-30 11:13:05 +0530

    Update and rename derp_fleur.mk to voltage_fleur.mk

--- voltage_fleur.mk
+++ voltage_fleur.mk
@@ -2,22 +2,18 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Derpfest stuff
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common Voltage stuff
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-# DerpFest Flags
+# Voltage Flags
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_ENABLE_BLUR := true
-TARGET_SUPPORTS_TOUCHGESTURES := true
-TARGET_GAPPS_ARCH := arm64
 
 # Inherit from fleur device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := derp_fleur
+PRODUCT_NAME := voltage_fleur
 PRODUCT_MODEL := Redmi Note 11S / Poco M4 Pro 4G
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_BOOT_ANIMATION_RES := 1080
