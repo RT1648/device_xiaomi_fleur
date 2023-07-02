7a56b76440098e1c5608e0ef1b8e88c5c7ac4b9f
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-07-02 08:45:06 +0530

    Update and rename voltage_fleur.mk to octavi_fleur.mk

--- octavi_fleur.mk
+++ octavi_fleur.mk
@@ -3,18 +3,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Voltage stuff
-$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 
 # Voltage Flags
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Inherit from fleur device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := voltage_fleur
+PRODUCT_NAME := octavi_fleur
 PRODUCT_MODEL := Redmi Note 11S
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
