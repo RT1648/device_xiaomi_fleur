e9b8435982d21bb3d700e50fe28d77ca4ad3b4c4
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-07-09 21:02:43 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_fleur.mk
+    $(LOCAL_DIR)/evolution_fleur.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_fleur-user \
-    havoc_fleur-userdebug \
-    havoc_fleur-eng
+    evolution_fleur-user \
+    evolution_fleur-userdebug \
+    evolution_fleur-eng
