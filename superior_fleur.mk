cd0f266eb49b9f308fa163055ab99b2f93731988
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-07-09 13:46:57 +0530

    Update and rename bliss_fleur.mk to superior_fleur.mk

--- superior_fleur.mk
+++ superior_fleur.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Bliss stuff
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common_full_phone.mk)
 
 # Bliss Flags
 TARGET_FACE_UNLOCK_SUPPORTED := true
@@ -14,7 +14,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := bliss_fleur
+PRODUCT_NAME := superior_fleur
 PRODUCT_MODEL := Redmi Note 11S / Poco M4 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
