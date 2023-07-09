a83402f2772e73afb39788e24051bd9505a17b58
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-07-09 14:00:42 +0530

    Update superior_fleur.mk

--- superior_fleur.mk
+++ superior_fleur.mk
@@ -2,10 +2,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Derpfest stuff
+# Inherit some common SuperiorOS stuff
 $(call inherit-product, vendor/superior/config/common_full_phone.mk)
 
-# Derpfest Flags
+# SuperiorOS Flags
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1440
 BUILD_WITH_GAPPS := true
