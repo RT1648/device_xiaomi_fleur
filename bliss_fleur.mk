a2ad3811d7c9c63132fe7134ce60d2b65486cfb3
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-07-09 13:25:22 +0530

    Update and rename aospa_fleur.mk to bliss_fleur.mk

--- bliss_fleur.mk
+++ bliss_fleur.mk
@@ -2,10 +2,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Havoc stuff
-$(call inherit-product, vendor/aospa/config/common_full_phone.mk)
+# Inherit some common Bliss stuff
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
-# Havoc Flags
+# Bliss Flags
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1440
 
@@ -14,7 +14,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := aospa_fleur
+PRODUCT_NAME := bliss_fleur
 PRODUCT_MODEL := Redmi Note 11S / Poco M4 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
