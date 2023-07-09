3724480ec309faee5f5b4ce53c187336e3a2e92e
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-07-09 21:04:38 +0530

    Update and rename havoc_fleur.mk to evolution_fleur.mk

--- evolution_fleur.mk
+++ evolution_fleur.mk
@@ -2,13 +2,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common HAVOCOS stuff
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+# Inherit some common Evolution-X stuff
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-# HAVOCOS Flags
+# Evolution-X Flags
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1440
-BUILD_WITH_GAPPS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_MINI_GAPPS := true
 TARGET_GAPPS_ARCH := arm64
 
 # Inherit from fleur device
@@ -16,7 +17,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := havoc_fleur
+PRODUCT_NAME := evolution_fleur
 PRODUCT_MODEL := Redmi Note 11S / Poco M4 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
