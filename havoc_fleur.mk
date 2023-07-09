b7f26081f96b1cf4db1522e8ea52199ce8e2d2a4
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-07-09 12:25:21 +0530

    Update and rename octavi_fleur.mk to havoc_fleur.mk

--- havoc_fleur.mk
+++ havoc_fleur.mk
@@ -2,23 +2,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Octavi stuff
-$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
+# Inherit some common Havoc stuff
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
-# Octavi Flags
+# Havoc Flags
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1440
-WITH_GAPPS := true
-TARGET_GAPPS_ARCH := arm64
-OCTAVI_MAINTAINER := Rohit
-TARGET_USES_BLUR := true
 
 # Inherit from fleur device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := octavi_fleur
+PRODUCT_NAME := havoc_fleur
 PRODUCT_MODEL := Redmi Note 11S / Poco M4 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
