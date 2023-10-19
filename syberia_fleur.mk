d2e165df178c929ef150aeafbe04298a2765a2d0
Rabikishan Rauniyar <141325011+rabikishan000@users.noreply.github.com>
2023-10-19 07:26:47 +0545

    Update and rename AICP_fleur.mk to syberia_fleur.mk

--- syberia_fleur.mk
+++ syberia_fleur.mk
@@ -2,8 +2,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common AICPOS stuff
-$(call inherit-product, vendor/AICP/config/common.mk)
+# Inherit some common SYBERIAOS stuff
+$(call inherit-product, vendor/syberia/config/common.mk)
 
 # Inherit from fleur device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -19,7 +19,7 @@ BUILD_WITH_GAPPS := true
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := AICP_fleur
+PRODUCT_NAME := syberia_fleur
 PRODUCT_MODEL := Redmi Note 11S 4G
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_BOOT_ANIMATION_RES := 1080
