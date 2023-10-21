e82c36481d98c20be58a52a2c54b7c06b3078409
Rabikishan Rauniyar <141325011+rabikishan000@users.noreply.github.com>
2023-10-21 20:52:25 +0545

    Fleur: yaapos Bring up

--- yaap_fleur.mk
+++ yaap_fleur.mk
@@ -2,8 +2,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common fuseOS stuff
-$(call inherit-product, vendor/fuse/config/common_full_phone.mk)
+# Inherit some common yaapOS stuff
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 # Inherit from fleur device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -19,7 +19,7 @@ BUILD_WITH_GAPPS := true
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := fuse_fleur
+PRODUCT_NAME := yaap_fleur
 PRODUCT_MODEL := Redmi Note 11S 4G
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_BOOT_ANIMATION_RES := 1080
