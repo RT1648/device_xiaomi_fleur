fb8229020879eebd555cfc07ebc373de72fb8a21
Roshan Kumar Rauniyar <139573813+roshanrauniyar@users.noreply.github.com>
2023-10-24 18:56:39 +0545

    Fleur: evox bring up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,13 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/blaze_fleur.mk 
-    #$(LOCAL_DIR)/evolution_fleur.mk
+    #$(LOCAL_DIR)/blaze_fleur.mk 
+    $(LOCAL_DIR)/evolution_fleur.mk
 
 COMMON_LUNCH_CHOICES := \
-    blaze_fleur-user \
-    blaze_fleur-userdebug \
-    blaze_fleur-eng 
-    #evolution_fleur-user \
-    #evolution_fleur-userdebug \
-    #evolution_fleur-eng
+    #blaze_fleur-user \
+    #blaze_fleur-userdebug \
+    #blaze_fleur-eng 
+    evolution_fleur-user \
+    evolution_fleur-userdebug \
+    evolution_fleur-eng
