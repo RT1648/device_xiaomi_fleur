b41acd0575e22f1834a183e5c5448a93fe5d8255
Roshan Kumar Rauniyar <139573813+roshanrauniyar@users.noreply.github.com>
2023-10-24 06:28:42 +0545

    Update and rename spark_fleur.mk to blaze_fleur.mk

--- blaze_fleur.mk
+++ blaze_fleur.mk
@@ -2,8 +2,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common sparkOS stuff
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+# Inherit some common blazeOS stuff
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
 # Inherit from fleur device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -23,7 +23,7 @@ WITH_GAPPS := true
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := spark_fleur
+PRODUCT_NAME := blaze_fleur
 PRODUCT_MODEL := Redmi Note 11S 4G
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_BOOT_ANIMATION_RES := 1080
