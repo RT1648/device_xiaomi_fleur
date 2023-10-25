2a6f21dab61f3256e6e7252eee23dce3467a8a44
Roshan Kumar Rauniyar <139573813+roshanrauniyar@users.noreply.github.com>
2023-10-25 08:33:09 +0545

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,13 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_fleur.mk
+    $(LOCAL_DIR)/lineage_fleur.mk
     #$(LOCAL_DIR)/blaze_fleur.mk 
 
 COMMON_LUNCH_CHOICES := \
-    evolution_fleur-user \
-    evolution_fleur-userdebug \
-    evolution_fleur-eng
+    lineage_fleur-user \
+    lineage_fleur-userdebug \
+    lineage_fleur-eng
     #blaze_fleur-user \
     #blaze_fleur-userdebug \
     #blaze_fleur-eng 
